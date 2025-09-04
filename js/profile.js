d469e90046c165b0fa29df347e7c2c0e9d89efd6
oumar zeini <oumaribnzeini7@gmail.com>
2025-09-04 17:58:56 +0100

    developing profile page

--- js/profile.js
+++ js/profile.js
@@ -1,13 +1,35 @@
 import { supabase, IP_INFO_API_KEY, WEATHER_API_KEY } from "./supabase.js";
 
-const displayLocation = () => {
-  const locationText = document.getElementById("user_location");
-  const locationInfo = JSON.parse(localStorage.getItem("locationInfo") || "[]");
-  locationText.textContent = `${locationInfo[0]} , ${locationInfo[1]}`;
-  localStorage.setItem("locationInfo", JSON.stringify(locationInfo));
+const userName = document.getElementById("user_name");
+const userLocation = document.getElementById("user_location");
+const userBio = document.getElementById("user_bio");
+const profileImage = document.getElementById("profile_img");
+const previewImg = document.getElementById("preview_img");
+profileImage.src = previewImg.src;
+
+if (!localStorage.getItem("customUserInfo")) {
+  const customUserInfo = {
+    profile_photo_url: null,
+    full_name: null,
+    location: null,
+    bio: null,
+  };
+  localStorage.setItem("customUserInfo", JSON.stringify(customUserInfo));
+}
+
+const toggleBioBtn = () => {
+  const customUserInfo = JSON.parse(
+    localStorage.getItem("customUserInfo") || "{}"
+  );
+
+  if (customUserInfo.bio === "") {
+    document.getElementById("add_bio_btn").style.display = "block";
+  } else {
+    document.getElementById("add_bio_btn").style.display = "none";
+  }
 };
 
-displayLocation();
+toggleBioBtn();
 
 const gettingNameAndEmail = async () => {
   const {
@@ -23,8 +45,13 @@ const gettingNameAndEmail = async () => {
     const emailInput = document.getElementById("email");
     emailInput.value = user.email;
 
-    const name = document.getElementById("user_name");
-    name.textContent = email.split("@")[0];
+    const customUserInfo = JSON.parse(
+      localStorage.getItem("customUserInfo") || "{}"
+    );
+
+    if (!customUserInfo.full_name) {
+      userName.textContent = JSON.parse(localStorage.getItem("userName"));
+    }
   }
 };
 
@@ -37,15 +64,15 @@ const getLocationWithIpinfo = async () => {
     const city = data.city;
     const region = data.region;
     const country = data.country;
-    const locationInfo = JSON.parse(
-      localStorage.getItem("locationInfo") || "[]"
+    const savedLocation = JSON.parse(
+      localStorage.getItem("userLocation") || "[]"
     );
-    locationInfo.push(city, country);
-    localStorage.setItem("locationInfo", JSON.stringify(locationInfo));
-  } catch (err) {
+    savedLocation.push(city, country);
+    localStorage.setItem("userLocation", JSON.stringify(savedLocation));
     const locationText = document.getElementById("user_location");
+    locationText.textContent = `${savedLocation[0]} , ${savedLocation[1]}`;
+  } catch (err) {
     console.log(`Error : `, err.message);
-    locationText.textContent = "Couldn't get location";
   }
 };
 
@@ -64,11 +91,13 @@ const getUserLocation = async () => {
         if (response.length > 0) {
           const city = response[0].name;
           const country = response[0].country;
-          const locationInfo = JSON.parse(
-            localStorage.getItem("locationInfo") || "[]"
+          const savedLocation = JSON.parse(
+            localStorage.getItem("userLocation") || "[]"
           );
-          locationInfo.push(city, country);
-          localStorage.setItem("locationInfo", JSON.stringify(locationInfo));
+          savedLocation.push(city, country);
+          localStorage.setItem("userLocation", JSON.stringify(savedLocation));
+          const locationText = document.getElementById("user_location");
+          locationText.textContent = `${savedLocation[0]} , ${savedLocation[1]}`;
         } else {
           console.log("No city found calling ipInfo fallback");
           getLocationWithIpinfo();
@@ -84,15 +113,133 @@ const getUserLocation = async () => {
   }
 };
 
-getUserLocation();
+const getLocation = async () => {
+  const savedLocation = JSON.parse(localStorage.getItem("userLocation"));
 
-const profilePhotoInput = document.getElementById("profile_photo");
-const profileImage = document.getElementById("profile_img");
-profilePhotoInput.addEventListener("change", (e) => {
-  if (e.target.files.length > 0) {
-    const file = e.target.files[0];
-    const prviewImg = document.getElementById("preview_img");
-    prviewImg.src = URL.createObjectURL(file);
-    profileImage.src = prviewImg.src;
+  if (savedLocation) {
+    const locationText = document.getElementById("user_location");
+    locationText.textContent = `${savedLocation[0]} , ${savedLocation[1]}`;
+    return;
+  } else {
+    getUserLocation();
   }
+};
+getLocation();
+
+const setProfileImage = () => {
+  const profilePhotoInput = document.getElementById("profile_photo");
+
+  profilePhotoInput.addEventListener("change", (e) => {
+    if (e.target.files.length > 0) {
+      const photo = e.target.files[0];
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const base64Image = e.target.result;
+
+        const customUserInfo = JSON.parse(
+          localStorage.getItem("customUserInfo") || "{}"
+        );
+        customUserInfo.profile_photo_url = base64Image;
+
+        previewImg.src = base64Image;
+
+        profileImage.src = base64Image;
+
+        localStorage.setItem("customUserInfo", JSON.stringify(customUserInfo));
+      };
+
+      reader.readAsDataURL(photo);
+      profilePhotoInput.value = "";
+    }
+  });
+};
+
+setProfileImage();
+
+const backArrow = document.getElementById("back_arrow");
+backArrow.onclick = () => {
+  document.getElementById("edit_profile_model").classList.remove("show");
+};
+
+const showEditProfilePage = () => {
+  const addBioBtn = document.getElementById("add_bio_btn");
+  addBioBtn.onclick = () => {
+    document.getElementById("edit_profile_model").classList.add("show");
+  };
+
+  const editProfileBtn = document.getElementById("edit_profile_btn");
+  editProfileBtn.onclick = () => {
+    document.getElementById("edit_profile_model").classList.add("show");
+  };
+
+  const customUserInfo = JSON.parse(
+    localStorage.getItem("customUserInfo") || "{}"
+  );
+
+  const currentName = document.getElementById("full_name");
+  currentName.value = customUserInfo.full_name;
+
+  const currentLocation = document.getElementById("location");
+  currentLocation.value = customUserInfo.location;
+
+  const newBio = document.getElementById("bio");
+  const currentBio = document.getElementById("user_bio");
+  if (customUserInfo.bio) {
+    newBio.value = customUserInfo.bio;
+  } else {
+    newBio.value = "";
+  }
+};
+
+showEditProfilePage();
+
+const editProfileForm = document.getElementById("edit_profile_form");
+editProfileForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const customUserInfo = JSON.parse(
+    localStorage.getItem("customUserInfo") || "{}"
+  );
+
+  const newName = document.getElementById("full_name").value.trim();
+
+  customUserInfo.full_name = newName;
+
+  const newLocation = document.getElementById("location").value;
+
+  customUserInfo.location = newLocation;
+
+  const newBio = document.getElementById("bio").value.trim();
+  customUserInfo.bio = newBio;
+
+  localStorage.setItem("customUserInfo", JSON.stringify(customUserInfo));
+
+  setInfo();
 });
+
+const setInfo = () => {
+  if (localStorage.getItem("customUserInfo")) {
+    const customUserInfo = JSON.parse(localStorage.getItem("customUserInfo"));
+
+    previewImg.src = customUserInfo.profile_photo_url;
+    profileImage.src = previewImg.src;
+    userName.textContent = customUserInfo.full_name;
+    userLocation.textContent = customUserInfo.location;
+    userBio.textContent = customUserInfo.bio;
+  }
+};
+
+setInfo();
+
+window.onload = () => {
+  const customUserInfo = JSON.parse(
+    localStorage.getItem("customUserInfo") || "{}"
+  );
+  if (customUserInfo.full_name) {
+    // previewImg.src = customUserInfo.profile_photo_url;
+    // profileImage.src = customUserInfo.profile_photo_url;
+    console.log(customUserInfo.full_name);
+    const name = document.getElementById("user_name");
+    name.textContent = customUserInfo.full_name;
+  }
+};
--- js/signup.js
+++ js/signup.js
@@ -1,7 +1,6 @@
 import { supabase } from "./supabase.js";
 
 const main = document.getElementById("main");
-console.log(main);
 if (window.innerWidth > 768) {
   main.classList.add("main");
   main.innerHTML = `
@@ -9,7 +8,7 @@ if (window.innerWidth > 768) {
         <form id="form">
           <div class="email_div">
             <label for="email">Email</label>
-            <input id="email" type="email" placeholder="name@example.com" />
+            <input id="email" type="email" placeholder="name@example.com" required />
           </div>
 
           <div class="password_div">
@@ -18,6 +17,7 @@ if (window.innerWidth > 768) {
               id="password"
               type="password"
               placeholder="Enter your password"
+              required
             />
           </div>
 
@@ -25,9 +25,9 @@ if (window.innerWidth > 768) {
         </form>
 
         <div class="bottom_div">
-          <button id="signup_btn">Sign Up</button>
+          <button id="to_signup_btn">Sign Up</button>
 
-          <p>Forgot Password ?</p>
+          <p id="forgot_password">Forgot Password ?</p>
         </div>
       </div>
 
@@ -36,12 +36,44 @@ if (window.innerWidth > 768) {
         <p class="motto">HowMuch , Traders never been happier...</p>
       </div>
   `;
-  const signUpBtn = document.getElementById("signup_btn");
+  const signUpBtn = document.getElementById("to_signup_btn");
   signUpBtn.onclick = () => {
     if (signUpBtn.textContent === "Sign Up") {
       signUpBtn.textContent = "Sign In";
       submitBtn.textContent = "Sign Up";
+
+      document.getElementById("forgot_password").style.display = "none";
+      const nameDiv = document.getElementById("full_name_div");
+
+      if (!nameDiv) {
+        const form = document.getElementById("form");
+
+        const nameDiv = document.createElement("div");
+        nameDiv.classList.add("full_name_div");
+        nameDiv.id = "full_name_div";
+
+        const nameLabel = document.createElement("label");
+        nameLabel.htmlFor = "full_name";
+        nameLabel.textContent = "Full Name";
+        //nameLabel.style.marginBottom = "10px";
+
+        const fullName = document.createElement("input");
+        fullName.id = "full_name";
+        fullName.type = "text";
+        fullName.placeholder = "ex. Med Ali";
+        fullName.required = true;
+
+        nameDiv.append(nameLabel, fullName);
+        form.prepend(nameDiv);
+      }
     } else {
+      const nameDiv = document.getElementById("full_name_div");
+      if (nameDiv) {
+        nameDiv.remove();
+      }
+
+      document.getElementById("forgot_password").style.display = "block";
+
       signUpBtn.textContent = "Sign Up";
       submitBtn.textContent = "Sign In";
     }
@@ -118,7 +150,7 @@ if (window.innerWidth > 768) {
       <form id="form">
         <div class="email_div">
           <label for="email">Email</label>
-          <input id="email" type="email" placeholder="Omarkam@example.com" />
+          <input id="email" type="email" placeholder="Omarkam@example.com" required/>
         </div>
 
         <div class="password_div">
@@ -127,52 +159,98 @@ if (window.innerWidth > 768) {
             id="password"
             type="password"
             placeholder="Enter your password"
+            required
           />
         </div>
 
-        <button type="submit" id="submit_btn">Sign In</button>
+        <button type="submit" id="submit_btn">  </button>
+       
       </form>
 
-      <div class="bottom_div">
+      <div id="forgot_password_div" class="bottom_div">
         <p>Forgot Password ?</p>
       </div>
 
       <button id="close_model" class="close_model">Close</button>
     </div>
   `;
+
+  const closeFeedback = document.getElementById("close_feedback");
+  closeFeedback.onclick = () => {
+    feedback.classList.remove("show");
+  };
+
+  const closeModel = document.getElementById("close_model");
+  closeModel.onclick = () => {
+    const model = document.getElementById("model");
+    model.classList.remove("show_model");
+    document.body.classList.remove("no_scroll");
+  };
+
+  const toSignUp = document.getElementById("to_signup_btn");
+  const toSignIn = document.getElementById("to_signin_btn");
+
+  toSignUp.onclick = () => {
+    const model = document.getElementById("model");
+    model.classList.add("show_model");
+    const submitBtn = document.getElementById("submit_btn");
+    submitBtn.textContent = "Sign Up";
+
+    document.body.classList.add("no_scroll");
+
+    document.getElementById("forgot_password_div").style.display = "none";
+
+    const nameDiv = document.getElementById("full_name_div");
+
+    if (!nameDiv) {
+      const form = document.getElementById("form");
+
+      const nameDiv = document.createElement("div");
+      nameDiv.classList.add("full_name_div");
+      nameDiv.id = "full_name_div";
+
+      const nameLabel = document.createElement("label");
+      nameLabel.htmlFor = "full_name";
+      nameLabel.textContent = "Full Name";
+      //nameLabel.style.marginBottom = "10px";
+
+      const fullName = document.createElement("input");
+      fullName.id = "full_name";
+      fullName.type = "text";
+      fullName.placeholder = "ex. Med Ali";
+      fullName.required = true;
+
+      nameDiv.append(nameLabel, fullName);
+      form.prepend(nameDiv);
+    }
+  };
+
+  toSignIn.onclick = () => {
+    const model = document.getElementById("model");
+    model.classList.add("show_model");
+    document.body.classList.add("no_scroll");
+
+    const submitBtn = document.getElementById("submit_btn");
+    submitBtn.textContent = "Sign In";
+    submitBtn.style.marginTop = "1rem";
+
+    document.getElementById("forgot_password_div").style.display = "block";
+
+    const nameDiv = document.getElementById("full_name_div");
+    if (nameDiv) {
+      nameDiv.remove();
+    }
+  };
 }
 
-const toSignUp = document.getElementById("to_signup_btn");
-const toSignIn = document.getElementById("to_signin_btn");
 const feedback = document.getElementById("feedback");
+
 const feedbackMessage = document.getElementById("feedback_message");
-const submitBtn = document.getElementById("submit_btn");
-const closeFeedback = document.getElementById("close_feedback");
-const closeModel = document.getElementById("close_model");
-closeModel.onclick = () => {
-  const model = document.getElementById("model");
-  model.classList.remove("show_model");
-};
-
-toSignUp.onclick = () => {
-  const model = document.getElementById("model");
-  model.classList.add("show_model");
-  submitBtn.textContent = "Sign Up";
-};
-
-toSignIn.onclick = () => {
-  const model = document.getElementById("model");
-  model.classList.add("show_model");
-  submitBtn.textContent = "Sign In";
-};
-
-closeFeedback.onclick = () => {
-  feedback.classList.remove("show");
-};
 
 const form = document.getElementById("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  const submitBtn = document.getElementById("submit_btn");
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
@@ -192,6 +270,9 @@ const signUp = async (email, password) => {
     console.log("signed up successfully.");
     feedback.classList.add("show");
     feedbackMessage.innerHTML = `<p>Please check your email for a verification link and sign in.</p> <svg class="feedback_icon" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 24 24" fill="#028a00"><path fill="none" stroke="#028a00" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0zM12 9v4m0 4h.01"/></svg>`;
+    const userName = document.getElementById("full_name").value.trim();
+    localStorage.setItem("userName", JSON.stringify(userName));
+    //console.log(JSON.parse(localStorage.getItem("userName")));
   }
 };
 
@@ -210,6 +291,6 @@ const signIn = async (email, password) => {
   }
 };
 
-// window.addEventListener("resize", () => {
-//   window.location.reload();
-// });
+window.addEventListener("resize", () => {
+  window.location.reload();
+});
